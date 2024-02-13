b235a975693b0d2cc227d66c9dd1486c6e07b69d
Scrappyz <ghostmic3000@gmail.com>
2024-02-13 13:37:43 +0800

    testing json

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,19 @@
 #include <vector>
 #include "json.hpp"
 
+using json = nlohmann::json;
+
 int main()
 {
     std::cout << "hello " << std::endl;
+    json j = json::parse(R"(
+        {
+            "pi": 3.14,
+            "happy": true
+        }
+    )");
+    std::ofstream f("file.json");
+    f << j;
+    f.close();
     return 0;
 }
