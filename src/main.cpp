5412f88ed95a041654d39b6d56a2b248a34277c0
Scrappyz <ghostmic3000@gmail.com>
2024-02-14 21:01:52 +0800

    added tests

--- src/main.cpp
+++ src/main.cpp
@@ -7,97 +7,97 @@
 using json = nlohmann::json;
 namespace path = os::path;
 
-// void play(const std::string& scenario_dir, const std::string& start_scenario)
-// {
-//     std::string scenario_file = path::joinPath(scenario_dir, start_scenario);
-//     std::unordered_map<std::string, int> modifiers;
-//     std::unordered_map<std::string, int> tries;
-//     std::ifstream f;
-//     json data;
+void play(const std::string& scenario_dir, const std::string& start_scenario)
+{
+    std::string scenario_file = path::joinPath(scenario_dir, start_scenario);
+    std::unordered_map<std::string, int> modifiers;
+    std::unordered_map<std::string, int> tries;
+    std::ifstream f;
+    json data;
 
-//     while(true) {
-//         f.open(scenario_file);
-//         data = json::parse(f);
-//         f.close();
+    while(true) {
+        f.open(scenario_file);
+        data = json::parse(f);
+        f.close();
 
-//         std::string scenario = data.at("scenario");
-//         std::cout << scenario << std::endl;
-//         std::cout << std::endl;
+        std::string scenario = data.at("scenario");
+        std::cout << scenario << std::endl;
+        std::cout << std::endl;
 
-//         if(data.at("gameOver")) {
-//             std::cout << "Game Over" << std::endl;
-//             break;
-//         }
+        if(data.at("gameOver")) {
+            std::cout << "Game Over" << std::endl;
+            break;
+        }
 
-//         std::vector<std::string> invalids = data.at("invalids");
-//         std::string input;
-//         std::string next_scenario;
+        std::vector<std::string> invalids = data.at("invalids");
+        std::string input;
+        std::string next_scenario;
 
-//         while(true) {
-//             std::cout << "> ";
-//             std::getline(std::cin, input);
+        while(true) {
+            std::cout << "> ";
+            std::getline(std::cin, input);
 
-//             std::string choice = getChoice(input, data.at("choices"));
+            std::string choice = getChoice(input, data.at("choices"));
 
-//             if(choice.empty()) {
-//                 std::cout << invalids[randomNumber(0, invalids.size()-1)] << std::endl;
-//                 std::cout << std::endl;
-//                 continue;
-//             }
+            if(choice.empty()) {
+                std::cout << invalids[randomNumber(0, invalids.size()-1)] << std::endl;
+                std::cout << std::endl;
+                continue;
+            }
 
-//             json choice_data = data.at("choices").at(choice);
-//             int max_tries = 1;
+            json choice_data = data.at("choices").at(choice);
+            int max_tries = 1;
 
-//             if(choice_data.contains("tries")) {
-//                 max_tries = choice_data.at("tries");
-//             }
+            if(choice_data.contains("tries")) {
+                max_tries = choice_data.at("tries");
+            }
 
-//             if(max_tries >= 0 && tries.count(choice) > 0 && tries.at(choice) >= max_tries) {
-//                 std::cout << invalids[randomNumber(0, invalids.size()-1)] << std::endl;
-//                 std::cout << std::endl;
-//                 continue;
-//             }
+            if(max_tries >= 0 && tries.count(choice) > 0 && tries.at(choice) >= max_tries) {
+                std::cout << invalids[randomNumber(0, invalids.size()-1)] << std::endl;
+                std::cout << std::endl;
+                continue;
+            }
 
-//             if(choice_data.contains("requires") && !hasModifiers(modifiers, choice_data.at("requires"))) {
-//                 std::cout << invalids[randomNumber(0, invalids.size()-1)] << std::endl;
-//                 std::cout << std::endl;
-//                 continue;
-//             }
+            if(choice_data.contains("requires") && !hasModifiers(modifiers, choice_data.at("requires"))) {
+                std::cout << invalids[randomNumber(0, invalids.size()-1)] << std::endl;
+                std::cout << std::endl;
+                continue;
+            }
 
-//             if(choice_data.contains("next")) {
-//                 next_scenario = choice_data.at("next");
-//             }
+            if(choice_data.contains("next")) {
+                next_scenario = choice_data.at("next");
+            }
             
-//             if(choice_data.contains("takes")) {
-//                 subtractModifiers(modifiers, choice_data.at("takes"));
-//             }
+            if(choice_data.contains("takes")) {
+                subtractModifiers(modifiers, choice_data.at("takes"));
+            }
 
-//             if(choice_data.contains("gives")) {
-//                 addModifiers(modifiers, choice_data.at("gives"));
-//             }
+            if(choice_data.contains("gives")) {
+                addModifiers(modifiers, choice_data.at("gives"));
+            }
 
-//             bool has_message = choice_data.contains("message");
-//             if(has_message) {
-//                 std::cout << std::string(choice_data.at("message")) << std::endl;
-//                 std::cout << std::endl;
-//             }
+            bool has_message = choice_data.contains("message");
+            if(has_message) {
+                std::cout << std::string(choice_data.at("message")) << std::endl;
+                std::cout << std::endl;
+            }
 
-//             tries[choice]++;
+            tries[choice]++;
 
-//             if(!next_scenario.empty()) {
-//                 break;
-//             }
+            if(!next_scenario.empty()) {
+                break;
+            }
 
-//             if(!has_message) {
-//                 std::cout << invalids[randomNumber(0, invalids.size()-1)] << std::endl;
-//                 std::cout << std::endl;
-//             }
-//         }
+            if(!has_message) {
+                std::cout << invalids[randomNumber(0, invalids.size()-1)] << std::endl;
+                std::cout << std::endl;
+            }
+        }
 
-//         tries.clear();
-//         scenario_file = path::joinPath(scenario_dir, path::appendFileExtension(next_scenario, "json"));
-//     }
-// }
+        tries.clear();
+        scenario_file = path::joinPath(scenario_dir, path::appendFileExtension(next_scenario, "json"));
+    }
+}
 
 int main()
 {
--- test/src/test.cpp
+++ test/src/test.cpp
@@ -1,7 +1,25 @@
 #include <gtest/gtest.h>
 #include "helper.hpp"
 
+using json = nlohmann::json;
+
 TEST(toLower, lowercase)
 {
-    EXPECT_EQ(toLower("pO"), "po");
+    EXPECT_EQ(toLower("QuaSi*MoTTO^"), "quasi*motto^");
+}
+
+TEST(parseChoice, split)
+{
+    EXPECT_EQ(parseChoice("wassup|wasabi|potato|shit head"), std::vector<std::string>({"wassup", "wasabi", "potato", "shit head"}));
+}
+
+TEST(getChoice, get)
+{
+    json j = {
+        {"wassup", ""},
+        {"kill", ""},
+        {"hi", ""}
+    };
+    EXPECT_EQ(getChoice("wassup there", j), "wassup");
+    EXPECT_EQ(getChoice("her kill", j), "kill");
 }
